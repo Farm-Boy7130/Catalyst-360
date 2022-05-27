d9c9e62b9dbb51527f1cc1888a683a7ab02e30e0
Farm-Boy7130 <104338902+Farm-Boy7130@users.noreply.github.com>
2022-05-26 20:52:28 -0500

    Create ReadMe.cpp

---
+++ ReadMe.cpp
@@ -0,0 +1,17 @@
+/*
+On Thursday, 5/26/2022, Project code-name "Catalyst 360" began development, This program will be much more diverse unlike the other programs I have on my GitHub.
+This program will feature a functional Graphical User Interface (GUI) and will be written in both C++ and C#. This will be a single dev program, with regular updates.
+Currently my plans are to release a Public Alpha v1.0.0 for testing, It will not fully feature a GUI until the official Full release of the program. The above statement
+is slightly misleading in this aspect, I apologize for that. However, I am a full time highschool student and an Entrepreneur and don't have all the time in the world.
+I will answer any questions I am asked in my discord server for this Open-Alpha testing phase. Public Alpha Testing is projected to start within the next week.
+Could be tomorrow, Could be 4 days from now. Regardless, Please be patient for bug fixes. With this out of the way, I will list the features for this program.
+
+Features for v1.0.0:
+- Full Metric and Imperial system support
+- Displacement and Compression ratio Calculator
+- Some Customization
+- Open-Sourced
+
+As of today, 5/26/22 I will be working on the first iteration of the program. Thank you for your patience!
+
+*/
