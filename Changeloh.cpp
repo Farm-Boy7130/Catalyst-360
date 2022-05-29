390e32001217713c6b9abd24b30eac4292f878d2
Farm-Boy7130 <104338902+Farm-Boy7130@users.noreply.github.com>
2022-05-28 23:34:01 -0500

    Create Changeloh.cpp

---
+++ Changeloh.cpp
@@ -0,0 +1,9 @@
+/*
+5/28/22:
+- I apologize for the delay. I have experienced several issues and I cannot release anything for a minute. Reasons include, Cannot build an exe file from the source code.
+I also am in the process of buying several new pieces of machinery, and I am also in the process of aquiring a new GPU, an Asus RTX 3060 12GB. Reasoning for buying this card are beyond simple explination.
+I can, and most likely will give a projected release date for the program. However, please keep in mind I am a single dev, going through a hard time as it is.
+I am also very busy only able to work on this one day out of the week. This will be an interesting process nonetheless, for the mean time...
+Projected alpha release date: Sunday, July 3rd, 2022. Please remember this can be sooner and later, this is just an estimate!
+Thank you all for your patience!
+*/
